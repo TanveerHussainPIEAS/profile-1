5727faf924a7b513609acffde1a5543222f103e2
Tanveer Hussain <thussain@cinnova.com>
2023-02-13 16:01:57 +0500

    ............

--- profile/src/app/app.module.ts
+++ profile/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ToolsComponent } from './tools/tools.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    ToolsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
